82b2d565aceeddc2e9ded9089100acff1c066dc1
Marta <113170762+marta-rakowska@users.noreply.github.com>
2023-01-17 21:51:36 +0100

    Update skyroute.py

--- skyroute.py
+++ skyroute.py
@@ -3,7 +3,6 @@ from vc_metro import vc_metro
 from vc_landmarks import vc_landmarks
 from landmark_choices import landmark_choices
 
-# Build your program below:
 landmark_string = ""
 for letter, landmark in landmark_choices.items():
   landmark_string += "{0} - {1}\n".format(letter, landmark)
